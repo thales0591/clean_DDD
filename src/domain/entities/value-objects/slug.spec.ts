98f80e48d7de51caf1976e1faf9e7270d5e90c47
thales0591 <thales0591@gmail.com>
2025-03-16 21:54:48 -0300

    feat: implement slug Value Object

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,9 @@
+import { expect, it } from 'vitest'
+import { Slug } from './slug'
+
+it('should be able to create a new slug from text', () => {
+  const slug = Slug.createFromText('Example question ti$tle'
+  )
+
+  expect(slug.value).toEqual('example-question-title')
+})
