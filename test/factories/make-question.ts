2093ef9d35eae342967d578e15248d1d7b3232b0
thales0591 <thales0591@gmail.com>
2025-03-19 13:58:26 -0300

    test: install faker-js to generate mock questions

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,14 +3,21 @@ import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: 'Example question',
-    authorId: new UniqueEntityId('id-01'),
-    content: 'Example content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId('id-01'),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
