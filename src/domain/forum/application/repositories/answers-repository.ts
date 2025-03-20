3fd5a098e7b86edf56d3642d6fdef81263b96990
thales0591 <thales0591@gmail.com>
2025-03-20 11:33:46 -0300

    feat: implement edit answer use case and its unit tests

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -3,5 +3,6 @@ import { Answer } from '../../enterprise/entities/answer'
 export interface AnswersRepository {
   findById(answerId: string): Promise<Answer | null>
   create(answer: Answer): Promise<void>
+  save(question: Answer): Promise<void>
   delete(answer: Answer): Promise<void>
 }
---
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -0,0 +1,54 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { EditAnswerUseCase } from './edit-answer'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: EditAnswerUseCase
+
+describe('Edit Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new EditAnswerUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to edit a answer', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityId('author-01'),
+      },
+      new UniqueEntityId('answer-01'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      answerId: newAnswer.id.toValue(),
+      authorId: 'author-01',
+      content: 'updated content',
+    })
+
+    expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+      content: 'updated content',
+    })
+  })
+
+  it('should not be able to edit a answer from another user', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityId('author-01'),
+      },
+      new UniqueEntityId('answer-01'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await expect(() =>
+      sut.execute({
+        answerId: newAnswer.id.toValue(),
+        authorId: 'author-02',
+        content: 'updated content',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
