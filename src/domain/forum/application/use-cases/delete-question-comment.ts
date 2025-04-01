91b599a60a162d519754c773ecd2f392a03a5952
thales0591 <thales0591@gmail.com>
2025-04-01 14:24:32 -0300

    feat: implement delete question comment use case and its unit tests

---
+++ src/domain/forum/application/use-cases/delete-question-comment.ts
@@ -0,0 +1,34 @@
+import { QuestionCommmentsRepository } from '../repositories/question-comments-repository'
+
+interface DeleteQuestionCommentUseCaseRequest {
+  authorId: string
+  questionCommentId: string
+}
+
+interface DeleteQuestionCommentUseCaseResponse {}
+
+export class DeleteQuestionCommentUseCase {
+  constructor(
+    private questionCommentsRepository: QuestionCommmentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    questionCommentId,
+  }: DeleteQuestionCommentUseCaseRequest): Promise<DeleteQuestionCommentUseCaseResponse> {
+    const questionComment =
+      await this.questionCommentsRepository.findById(questionCommentId)
+
+    if (!questionComment) {
+      throw new Error('Question comment not found.')
+    }
+
+    if (questionComment.authorId.toString() !== authorId) {
+      throw new Error('Not allowed')
+    }
+
+    await this.questionCommentsRepository.delete(questionComment)
+
+    return {}
+  }
+}
