b4ff3c21b865caa55175b342046328e5613037bf
thales0591 <thales0591@gmail.com>
2025-04-01 21:45:13 -0300

    feat: implement question comments use case and its unit tests

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,7 +1,13 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionComment } from '../../enterprise/entities/question-comment'
 
 export interface QuestionCommmentsRepository {
   create(questionComment: QuestionComment): Promise<void>
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<QuestionComment[]>
+
   delete(questionComment: QuestionComment): Promise<void>
   findById(id: string): Promise<QuestionComment | null>
 }
---
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -0,0 +1,31 @@
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { QuestionCommmentsRepository } from '../repositories/question-comments-repository'
+
+interface FetchQuestionCommentsUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionCommentsUseCaseResponse {
+  questionComments: QuestionComment[]
+}
+
+export class FetchQuestionCommentsUseCase {
+  constructor(
+    private questionCommentsRepository: QuestionCommmentsRepository,
+  ) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
+    const questionComments =
+      await this.questionCommentsRepository.findManyByQuestionId(questionId, {
+        page,
+      })
+
+    return {
+      questionComments,
+    }
+  }
+}
